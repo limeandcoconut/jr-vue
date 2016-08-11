11e822c5b8b70e28a4ac31fcc051e31b241a7844
Jacob Smith <messagethesmith@gmail.com>
2016-08-10 19:38:53 -0700

    Add hover styles to achievements and footer. Completely rework hover mechanics. Dirty commit.

--- client/components/home.vue
+++ client/components/home.vue
@@ -21,12 +21,13 @@
                 </div>
                 <div class="section__main">
                     <a v-link="'/project/' + project.projectUrl" v-for="project in projects" class="project">
-                        <div v-if="project.projectMainImage.src" class="project__img-container">
+                        <div v-if="project.projectMainImage.src" class="project__img-container hover-highlight">
                             <img :src="project.projectMainImage.src" :alt="project.projectMainImage.alt" class="section__image project__img">
-                            <div @mousemove="recordHover" class="project__img-overlay" :index="$index">
-                                <div @mousemove.stop="recordHoverFromChild" class="project__overlay-text" v-show="activeHover == $index" :style="{ left: overlayLeft, top: overlayTop }">
+                            <!-- <div @mousemove="recordHover" class="project__img-overlay" :index="$index"> -->
+                                <div  class="project__overlay-text hover-highlight__follow"  :style="{ left: overlayLeft, top: overlayTop }">
                                     {{ project.projectName }}
                                 </div>
+                                <div @mousemove="recordHover"  class="project__hover-target hover-highlight__target"></div>
                             </div>
                         </div>
                     </a>
@@ -60,7 +61,37 @@
                 </div>
                 <div class="section__main">
                     <div class="section__copy">
-                        Graduated Bible College in Hungry, EU. I was a <span class="link link--underline">youth pastor</span>. <span class="link link--underline">Graduated</span> college with a Bacholars in Web. I bought a <span class="link link--underline">house</span>. I got <span class="link link--underline">married</span> and soon to be a <span class="link link--underline">dad</span>.
+
+                        Graduated Bible College in Hungry, EU. I was a
+                        <span class="hover-highlight hover-highlight--inline link link--underlit">
+                            youth pastor
+                            <img class="hover-highlight__follow hover-highlight__follow--img" src="/img/highlight-youth-pastor.jpg" :style="{ left: overlayLeft, top: overlayTop }"/>
+                            <span class="hover-highlight__target hover-highlight__target--inline" @mousemove="recordHover">target</span>
+                        </span>
+                        . Graduated college with a
+                        <span class="hover-highlight hover-highlight--inline link link--underlit">
+                            Bacholars in Web
+                            <img class="hover-highlight__follow hover-highlight__follow--img" src="/img/highlight-graduated.jpg" :style="{ left: overlayLeft, top: overlayTop }"/>
+                            <span class="hover-highlight__target hover-highlight__target--inline" @mousemove="recordHover">target</span>
+                        </span>
+                        . I bought a
+                        <span class="hover-highlight hover-highlight--inline link link--underlit">
+                            house
+                            <img class="hover-highlight__follow hover-highlight__follow--img" src="/img/highlight-house.jpg" :style="{ left: overlayLeft, top: overlayTop }"/>
+                            <span class="hover-highlight__target hover-highlight__target--inline" @mousemove="recordHover">target</span>
+                        </span>
+                         I got
+                        <span class="hover-highlight hover-highlight--inline link link--underlit">
+                            married
+                            <img class="hover-highlight__follow hover-highlight__follow--img" src="/img/highlight-married.jpg" :style="{ left: overlayLeft, top: overlayTop }"/>
+                            <span class="hover-highlight__target hover-highlight__target--inline" @mousemove="recordHover">target</span>
+                        </span>
+                        and soon to be a
+                        <span class="hover-highlight hover-highlight--inline link link--underlit">
+                            dad
+                            <img class="hover-highlight__follow hover-highlight__follow--img" src="/img/highlight-dad.jpg" :style="{ left: overlayLeft, top: overlayTop }"/>
+                            <span class="hover-highlight__target hover-highlight__target--inline" @mousemove="recordHover">target</span>
+                        </span>
                     </div>
                 </div>
             </section>
@@ -104,13 +135,25 @@ export default {
             this.recordedHoverLeft = event.offsetX;
             this.recordedHoverTop = event.offsetY;
 
+            // console.log(event.offsetX. ' ' '
+            // );
+            console.log(this.recordedHoverLeft);
+
             window.raf(this.followText);
         },
         recordHoverFromChild(event) {
             this.recordedActiveHover = event.srcElement.parentElement.getAttribute('index');
 
             this.recordedHoverLeft = event.offsetX + event.target.offsetLeft - (event.target.offsetWidth / 2);
-            this.recordedHoverTop = event.offsetY + event.target.offsetTop - (event.target.offsetHeight / 3);
+            this.recordedHoverTop = event.offsetY + event.target.offsetTop - (event.target.offsetHeight / 2);
+
+            // console.log(event.offsetX, '  ', this.recordedHoverLeft + 150);
+            // console.log(event.offsetY, '  ', this.recordedHoverTop);
+
+            // console.log(event.offsetX);
+            // console.log(event.target.offsetLeft);
+            // console.log(event.target.offsetWidth/2);
+            // console.log(event.target.offsetHeight);
 
             window.raf(this.followText);
             event.stopPropagation();
--- client/components/layouts/default_footer.vue
+++ client/components/layouts/default_footer.vue
@@ -1,8 +1,9 @@
 <template>
-    <footer id="footer">
+    <footer id="footer" class="hover-highlight">
         <!-- <div class="footer__copyright"> -->
             <!-- Copyright &copy; {{ moment(new Date()).format('YYYY') }} -->
-            <a href="mailto:james@snapshot.is" class="footer__mailto">Contact</a>
+            <div class="footer__mailto hover-highlight__follow" :style="{ left: overlayLeft, top: overlayTop }">Contact</div>
+            <a href="mailto:james@snapshot.is" class="hover-highlight__target" @mousemove="recordHover"></a>
         <!-- </div> -->
     </footer>
 </template>
@@ -11,10 +12,51 @@
     import moment from '../../libs/moment.min.js';
     export default {
         name: 'footer',
+        data() {
+            return {
+                overlayLeft: 0,
+                overlayTop: 0,
+                activeHover: 0,
+            };
+        },
         methods: {
             moment: function(date) {
                 return moment(date);
             },
-        },
+            recordHover(event) {
+                this.recordedActiveHover = event.srcElement.getAttribute('index');
+                this.recordedHoverLeft = event.offsetX;
+                this.recordedHoverTop = event.offsetY;
+
+                // console.log(event.offsetX. ' ' '
+                // );
+                console.log(this.recordedHoverLeft);
+
+                window.raf(this.followText);
+            },
+            recordHoverFromChild(event) {
+                this.recordedActiveHover = event.srcElement.parentElement.getAttribute('index');
+
+                this.recordedHoverLeft = event.offsetX + event.target.offsetLeft - (event.target.offsetWidth / 2);
+                this.recordedHoverTop = event.offsetY + event.target.offsetTop - (event.target.offsetHeight / 2);
+
+                // console.log(event.offsetX, '  ', this.recordedHoverLeft + 150);
+                // console.log(event.offsetY, '  ', this.recordedHoverTop);
+
+                // console.log(event.offsetX);
+                // console.log(event.target.offsetLeft);
+                // console.log(event.target.offsetWidth/2);
+                // console.log(event.target.offsetHeight);
+
+                window.raf(this.followText);
+                event.stopPropagation();
+                return false;
+            },
+            followText() {
+                this.overlayLeft = this.recordedHoverLeft + 'px';
+                this.overlayTop = this.recordedHoverTop + 'px';
+                this.activeHover = this.recordedActiveHover;
+            }
+        }
     };
 </script>
